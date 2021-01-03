3653611ae7a39d4fcd420e3c76e129c3a471bf8f
Layton Whiteley <layton.w@smart-host.com>
2021-01-03 08:56:54 +0100

    build: update tsc process

--- src/__tests__/main.spec.ts
+++ src/__tests__/main.spec.ts
@@ -1,4 +1,4 @@
-import {wait} from '../src/wait'
+import {wait} from '../wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
@@ -12,7 +12,7 @@ test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
+  const delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
 })
 
@@ -20,7 +20,7 @@ test('wait 500 ms', async () => {
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
   const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const ip = path.join(__dirname, '../../', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
